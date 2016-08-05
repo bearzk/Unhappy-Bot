c36dd94dfd82a554496c88edf0dbc96f09a839aa
Kai Zhang <kai.zhangxa@gmail.com>
2016-08-05 15:00:08 +0200

    Command only allowed by user_id

--- command_callbacks.py
+++ command_callbacks.py
@@ -1,3 +1,4 @@
+from config import Config
 from telegram.ext.dispatcher import run_async
 
 @run_async
@@ -10,3 +11,11 @@ def whoami(bot, update):
     text = "Your name is %s %s\n" % (user.first_name, user.last_name)
     text += "Your id is %s" % user.id
     bot.sendMessage(chat_id=update.message.chat_id, text=text)
+
+@run_async
+def scream(bot, update):
+    user = update.message.from_user
+    message = "You are not allowed here"
+    if user.id in Config['authorized']:
+        message = "You should be happy"
+    bot.sendMessage(chat_id=update.message.chat_id, text=message)
--- unhappy_bot.py
+++ unhappy_bot.py
@@ -2,7 +2,7 @@ import logging
 from config import Config
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-from command_callbacks import start, whoami
+from command_callbacks import start, whoami, scream
 from message_callbacks import echo
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -14,11 +14,13 @@ dispatcher = updater.dispatcher
 
 start_handler = CommandHandler('start', start)
 whoami_handler = CommandHandler('whoami', whoami)
+scream_handeler = CommandHandler('scream', scream)
 
 echo_handler = MessageHandler([Filters.text], echo)
 
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(whoami_handler)
+dispatcher.add_handler(scream_handeler)
 dispatcher.add_handler(echo_handler)
 
 updater.start_polling()
