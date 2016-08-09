7c4e071d8b12e85ba8674c6ed81eee3657460041
bearzk <kai.zhangxa@gmail.com>
2016-08-09 23:05:10 +0200

    Add translation
    source glosbe.com

--- command_callbacks.py
+++ command_callbacks.py
@@ -1,9 +1,10 @@
 from os import system
 from datetime import timedelta
 
-from config import Config
 from telegram.ext.dispatcher import run_async
+import requests
 
+from config import Config
 from callback_decorators import authorized
 
 @run_async
@@ -25,3 +26,18 @@ def stats(bot, update):
         uptime_string = str(timedelta(seconds = uptime_seconds))
 
     bot.sendMessage(chat_id=update.message.chat_id, text=uptime_string)
+
+@run_async
+def translate(bot, update):
+    source = update.message.text.replace('/trans ', '').strip()
+    if source:
+        query = "https://glosbe.com/gapi/translate?from=deu&dest=eng&format=json&phrase=%s" % source.lower()
+        res = requests.get(query)
+        translations = res.json()['tuc']
+        if translations:
+            trans = [t['phrase']['text'] for t in translations if t.has_key('phrase')]
+            message = "Translations found for word %s :\n" % source
+            message += "\n".join(trans)
+        else:
+            message = "No translation found for word %s" % source
+        bot.sendMessage(chat_id=update.message.chat_id, text=message)
--- unhappy_bot.py
+++ unhappy_bot.py
@@ -2,7 +2,7 @@ import logging
 from config import Config
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-from command_callbacks import start, whoami, stats
+from command_callbacks import start, whoami, stats, translate
 from message_callbacks import echo
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -15,12 +15,14 @@ dispatcher = updater.dispatcher
 start_handler = CommandHandler('start', start)
 whoami_handler = CommandHandler('whoami', whoami)
 stats_handeler = CommandHandler('stats', stats)
+translate_handler = CommandHandler('trans', translate)
 
 echo_handler = MessageHandler([Filters.text], echo)
 
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(whoami_handler)
 dispatcher.add_handler(stats_handeler)
+dispatcher.add_handler(translate_handler)
 
 dispatcher.add_handler(echo_handler)
 
